cebf6088fb30d967f685a39374a7c2d6ea14603a
Zer0 <114886018+Zer0G0ld@users.noreply.github.com>
2024-02-05 19:47:30 +0000

    Add melhorias

---
+++ src/js/script.js
@@ -0,0 +1,8 @@
+document.getElementById("singInButton").onclick = function () {
+    window.location.href = "./Login/SingIn/index.html";
+};
+
+document.getElementById("singUpButton").onclick = function () {
+    window.location.href = "./Login/SingUp/index.html";
+};
+
